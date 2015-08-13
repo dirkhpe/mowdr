89cd52b7e1f911e42994be7a1be8cb5ac2217c3f
unknown <vermeyle@VERMEYLE3.emea.hpqcorp.net>
2015-08-13 18:27:57 +0200

    Update

--- Python/BuildDatabase.py
+++ Python/BuildDatabase.py
@@ -23,27 +23,27 @@ def get_modulename():
     """
     # Extract calling application name
     (filepath, filename) = os.path.split(sys.argv[0])
-    (modulename, fileext) = os.path.splitext(filename)
-    return modulename
+    (module, fileext) = os.path.splitext(filename)
+    return module
 
 
 def get_inifile():
     # Use Project Name as ini file.
     projectname = 'mowdr'
     configfile = "properties/" + projectname + ".ini"
-    config = configparser.ConfigParser()
+    ini_config = configparser.ConfigParser()
     try:
-        config.read_file(open(configfile))
+        ini_config.read_file(open(configfile))
     except:
         e = sys.exc_info()[1]
         ec = sys.exc_info()[0]
         log_msg = "Read Inifile not successful: %s (%s)"
         print(log_msg % (e, ec))
         sys.exit(1)
-    return config
+    return ini_config
 
 
-def get_logfilename(modulename, config):
+def get_logfilename():
     """
     Temporary function to define Logfile Name.
     :return: Name of the logfile.
@@ -54,14 +54,14 @@ def get_logfilename(modulename, config):
     # Extract Computername
     computername = os.environ.get("COMPUTERNAME")
     # Define logfileName
-    logfilename = logdir + "/" + modulename + "_" + computername + \
+    logfile = logdir + "/" + modulename + "_" + computername + \
         "_" + currdate + ".log"
-    return logfilename
+    return logfile
 
 
-def connect2db(db=None):
+def connect2db():
     try:
-        conn = sqlite3.connect(db)
+        db_conn = sqlite3.connect(db)
     except:
         e = sys.exc_info()[1]
         ec = sys.exc_info()[0]
@@ -69,10 +69,10 @@ def connect2db(db=None):
         logging.error(log_msg, e, ec)
         return
     else:
-        return conn
+        return db_conn
 
 
-def create_db(conn):
+def create_db():
     # Create table
     query = 'CREATE TABLE attribute_action ' \
             '(id integer primary key, attribute text, od_field text, ' \
@@ -100,7 +100,7 @@ def create_db(conn):
     return True
 
 
-def remove_tables(conn):
+def remove_tables():
     query = 'DROP TABLE IF EXISTS indicators'
     try:
         conn.execute(query)
@@ -121,7 +121,7 @@ def remove_tables(conn):
         return
 
 
-def populate_attribs_main(conn):
+def populate_attribs_main():
     """
     This procedure will populate table attribute_action with the attributes that come from Dataroom
     and need to go to Dataset Metadata screen, Main.
@@ -129,6 +129,7 @@ def populate_attribs_main(conn):
     """
     attrib_od_fields = {
         'Title': 'Title',
+        'notes': 'notes',
     }
     # cur = conn.cursor()
     query = "INSERT INTO attribute_action (attribute, od_field, action, source, target, created) " \
@@ -146,7 +147,7 @@ def populate_attribs_main(conn):
     return
 
 
-def populate_attribs_extra(conn):
+def populate_attribs_extra():
     """
     This procedure will populate table attribute_action with the attributes that come from Dataroom
     and need to go to Dataset Metadata screen, section 'Extra Informatie'.
@@ -180,7 +181,7 @@ def populate_attribs_extra(conn):
     conn.commit()
 
 
-def populate_attribs_ckan(conn):
+def populate_attribs_ckan():
     """
     This procedure will populate table attribute_action with the attributes that come from ckan Open Data
     platform.
@@ -193,7 +194,7 @@ def populate_attribs_ckan(conn):
     }
     # cur = conn.cursor()
     query = "INSERT INTO attribute_action (attribute, od_field, action, source, target, created) " \
-            "VALUES (?, ?, 'DatasetIdentification', 'Dataset', 'Dataset', ?)"
+            "VALUES (?, ?, 'Main', 'Dataset', 'Dataset', ?)"
     for attribute, od_field in attrib_od_fields.items():
         try:
             conn.execute(query, (attribute, od_field, now,))
@@ -206,22 +207,25 @@ def populate_attribs_ckan(conn):
     conn.commit()
 
 
-def populate_attribs_mv(conn):
+def populate_attribs_mv():
     """
     This procedure will populate table attribute_action with the attributes that come from Mobiel Vlaanderen
     platform.
     :return:
     """
     attrib_od_fields = {
-        'url_cijfers': 'url_cijfers',
-        'url_commentaar': 'url_commentaar',
+        'url_cijfers': 'url',
+        'url_commentaar': 'url',
     }
-    # cur = conn.cursor()
     query = "INSERT INTO attribute_action (attribute, od_field, action, source, target, created) " \
-            "VALUES (?, ?, 'Repository', 'Repository', 'Dataset', ?)"
+            "VALUES (?, ?, 'Resource', 'Repository', ?, ?)"
     for attribute, od_field in attrib_od_fields.items():
+        if 'cijfers' in attribute:
+            target = 'CijfersResource'
+        else:
+            target = 'CommentaarResource'
         try:
-            conn.execute(query, (attribute, od_field, now,))
+            conn.execute(query, (attribute, od_field, target, now,))
         except:
             e = sys.exc_info()[1]
             ec = sys.exc_info()[0]
@@ -231,32 +235,65 @@ def populate_attribs_mv(conn):
     conn.commit()
 
 
-def main():
-    # Get ini-file first.
-    modulename = get_modulename()
-    config = get_inifile()
-    # Now configure logfile
-    logfilename = get_logfilename(modulename, config)
-    logging.basicConfig(format='%(asctime)s:%(module)s:%(funcName)s:%(lineno)d:%(levelname)s:%(message)s',
-                        datefmt='%d/%m/%Y %H:%M:%S', filename=logfilename, level=logging.DEBUG)
-    logging.info('Start Application')
-    logging.info('Get Database connection')
-    db = config['Main']['db']
-    conn = connect2db(db)
-    logging.info('Remove existing tables')
-    remove_tables(conn)
-    logging.info('Create the tables')
-    create_db(conn)
-    logging.info('Add Main Attributes')
-    populate_attribs_main(conn)
-    logging.info('Add Extra Attributes')
-    populate_attribs_extra(conn)
-    logging.info('Add ckan Attributes')
-    populate_attribs_ckan(conn)
-    logging.info('Add FTP (mobiel vlaanderen) Attributes')
-    populate_attribs_mv(conn)
-    conn.close()
-    logging.info('End Application')
+def populate_attribs_resource():
+    """
+    This procedure will populate table attribute_action with the attributes that need to go to cijfers/commentaar
+    Resources.
+    :return:
+    """
+    attrib_od_fields = {
+        'format_cijfers': 'format',
+        'name_cijfers': 'name',
+        'description_cijfers': 'description',
+        'id_cijfers': 'id',
+        'format_commentaar': 'format',
+        'name_commentaar': 'name',
+        'description_commentaar': 'description',
+        'id_commentaar': 'id',
+    }
+    query = "INSERT INTO attribute_action (attribute, od_field, action, source, target, created) " \
+            "VALUES (?, ?, 'Resource', 'Dataroom', ?, ?)"
+    for attribute, od_field in attrib_od_fields.items():
+        if 'cijfers' in attribute:
+            target = 'CijfersResource'
+        else:
+            target = 'CommentaarResource'
+        try:
+            conn.execute(query, (attribute, od_field, target, now,))
+        except:
+            e = sys.exc_info()[1]
+            ec = sys.exc_info()[0]
+            log_msg = "Error during query execution: %s %s"
+            logging.error(log_msg, e, ec)
+            return
+    conn.commit()
 
-if __name__ == '__main__':
-    main()
+
+# Get ini-file first.
+modulename = get_modulename()
+config = get_inifile()
+# Now configure logfile
+logfilename = get_logfilename()
+logging.basicConfig(format='%(asctime)s:%(module)s:%(funcName)s:%(lineno)d:%(levelname)s:%(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S', filename=logfilename, level=logging.DEBUG)
+logging.info('Start Application')
+logging.info('Get Database connection')
+db = config['Main']['db']
+conn = connect2db()
+logging.info('Remove existing tables')
+remove_tables()
+logging.info('Create the tables')
+create_db()
+logging.info('Add Main Attributes')
+populate_attribs_main()
+logging.info('Add Extra Attributes')
+populate_attribs_extra()
+logging.info('Add ckan Attributes')
+populate_attribs_ckan()
+logging.info('Add FTP (mobiel vlaanderen) Attributes')
+populate_attribs_mv()
+logging.info('Add Cijfer/Commentaar Resource Attributes')
+populate_attribs_resource()
+conn.close()
+logging.info('End Application')
+
--- Python/HandleFile.py
+++ Python/HandleFile.py
@@ -5,15 +5,17 @@ This is the main application to handle files from Dataroom and
 process them for loading on the Open Data Platform.
 """
 
+import ckanapi
 import configparser
 import datetime
 from ftplib import FTP
 import logging
 import os
 import re
+import sqlite3
 import sys
-# import ManageOpenData as Mod
-# import ManageResource as Mr
+from time import strftime
+import xml.etree.ElementTree as Et
 
 
 def get_modulename():
@@ -23,27 +25,27 @@ def get_modulename():
     """
     # Extract calling application name
     (filepath, filename) = os.path.split(sys.argv[0])
-    (modulename, fileext) = os.path.splitext(filename)
-    return modulename
+    (module, fileext) = os.path.splitext(filename)
+    return module
 
 
 def get_inifile():
     # Use Project Name as ini file.
     projectname = 'mowdr'
     configfile = "properties/" + projectname + ".ini"
-    config = configparser.ConfigParser()
+    ini_config = configparser.ConfigParser()
     try:
-        config.read_file(open(configfile))
+        ini_config.read_file(open(configfile))
     except:
         e = sys.exc_info()[1]
         ec = sys.exc_info()[0]
         log_msg = "Read Inifile not successful: %s (%s)"
         print(log_msg % (e, ec))
         sys.exit(1)
-    return config
+    return ini_config
 
 
-def get_logfilename(modulename, config):
+def get_logfilename():
     """
     Temporary function to define Logfile Name.
     :return: Name of the logfile.
@@ -54,18 +56,35 @@ def get_logfilename(modulename, config):
     # Extract Computername
     computername = os.environ.get("COMPUTERNAME")
     # Define logfileName
-    logfilename = logdir + "/" + modulename + "_" + computername + \
+    logfile = logdir + "/" + modulename + "_" + computername + \
         "_" + currdate + ".log"
-    return logfilename
+    return logfile
 
 
-def ftp_connection(host, user, passwd):
+def connect2db():
+    db = config['Main']['db']
+    try:
+        db_conn = sqlite3.connect(db)
+    except:
+        e = sys.exc_info()[1]
+        ec = sys.exc_info()[0]
+        log_msg = "Error during connect to database: %s %s"
+        logging.error(log_msg, e, ec)
+        return
+    else:
+        return db_conn
+
+
+def ftp_connection():
     """
     This procedure establishes an FTP connection to the FTP Server.
     :return: ftp object.
     """
     log_msg = "Trying to establish FTP Connection"
     logging.debug(log_msg)
+    host = config['FTPServer']['host']
+    user = config['FTPServer']['user']
+    passwd = config['FTPServer']['passwd']
     try:
         ftp = FTP(host=host, user=user, passwd=passwd)
     except:
@@ -174,11 +193,258 @@ def indic_from_file(filename):
     return indic_id
 
 
-def scan_for_files(config):
+def url_in_db(file):
+    """
+    Update table indicators with url information. If filename is empty, then remove entry from table.
+    Else add URL to the table.
+    :param file:
+    :return:
+    """
+    logging.debug('Add/Remove file %s to indicators table.', file)
+    indic_id = indic_from_file(file)
+    now = strftime("%H:%M:%S %d-%m-%Y")
+    if 'cijfers' in file:
+        attribute = 'url_cijfers'
+    else:
+        attribute = 'url_commentaar'
+    # Always prepare and run query for delete. Then no INSERT/UPDATE logic is required.
+    query = "DELETE FROM indicators WHERE indicator_id = ? AND attribute = ?"
+    conn.execute(query, (indic_id, attribute))
+    if 'empty' not in file:
+        ftp_home = config['FTPServer']['ftp_home']
+        url = ftp_home + '/' + file
+        query = "INSERT INTO indicators (indicator_id, attribute, value, created)" \
+                "VALUES (?, ?, ?, ?)"
+        conn.execute(query, (indic_id, attribute, url, now))
+    conn.commit()
+    return
+
+
+def load_metadata(metafile, indic_id, ckan_conn):
+    """
+    Read the file with metadata and add or replace the information to table indicators.
+    Call function to create dataset if this is a new dataset.
+    :param metafile: pointer to the file with metadata.
+    :return:
+    """
+    log_msg = "In load_metadata for file " + metafile
+    logging.debug(log_msg)
+    now = strftime("%H:%M:%S %d-%m-%Y")
+    try:
+        tree = Et.parse(metafile)
+    except:  # catch all errors for now, try to be more specific in the future.
+        e = sys.exc_info()[1]
+        ec = sys.exc_info()[0]
+        log_msg = "Error during parsing metafile xml: %s %s"
+        logging.critical(log_msg, e, ec)
+        return
+    root = tree.getroot()
+    # metadata is available, get list of attributes from Dataroom Application
+    # and required for Dataset Page.
+    # First collect all attribute names
+    query = "SELECT attribute FROM attribute_action " \
+            "WHERE source = 'Dataroom' AND target = 'Dataset'"
+    # cur = conn.cursor()
+    cur.execute(query)
+    attribs = cur.fetchall()
+    attrib_names = []
+    for row in attribs:
+        attrib_names.append(row[0])
+    # Then remove information from Dataroom for Dataset for this indicator ID.
+    query = "DELETE FROM indicators WHERE indicator_id = ? AND attribute = ?"
+    for attrib_name in attrib_names:
+        cur.execute(query, (indic_id, attrib_name))
+    # And then add new information from Dataroom for Dataset for this indicator ID.
+    query = "INSERT INTO indicators (indicator_id, attribute, value, created) VALUES (?, ?, ?, ?)"
+    for child in root:
+        if child.tag in attrib_names:
+            cur.execute(query, (indic_id, child.tag, child.text.strip(), now))
+            # Add 'notes' field (copy of definitie)
+            if child.tag.lower() == 'definitie':
+                cur.execute(query, (indic_id, 'notes', child.text.strip(), now))
+        elif child.tag != 'id':
+            log_msg = "Found Dataroom Attribute **" + child.tag + "** not required for Open Data Dataset"
+            logging.warning(log_msg)
+    conn.commit()
+    # Now check if dataset exist already: is there an ID available in the indicators table for this indicator.
+    query = "SELECT value FROM indicators WHERE attribute = 'id' and indicator_id = ?"
+    cur.execute(query, (indic_id,))
+    values_lst = cur.fetchall()
+    # I want to have 0 or 1 rows in the list
+    if len(values_lst) == 0:
+        log_msg = "Open Data dataset is not registered for Indicator ID %s, call to register"
+        logging.info(log_msg, indic_id)
+        create_package(indic_id, ckan_conn)
+    elif len(values_lst) == 1:
+        log_msg = "Open Data dataset exists for Indicator ID %s, no further action"
+        logging.info(log_msg, indic_id)
+    else:
+        log_msg = "Multiple Open Data dataset links found for Indicator ID %s, please review"
+        logging.warning(log_msg, indic_id)
+    update_package(indic_id, ckan_conn)
+    return True
+
+
+def create_package(indic_id, ckan_conn):
+    log_msg = "In create_package for Indicator %s"
+    logging.info(log_msg, indic_id)
+    # Get mandatory items for the package: Title, name and owner_org
+    now = strftime("%H:%M:%S %d-%m-%Y")
+    query = "SELECT value FROM indicators WHERE indicator_id = ? AND attribute = 'Title'"
+    # cur = conn.cursor()
+    cur.execute(query, (indic_id,))
+    title_list = cur.fetchall()
+    # I need to have exact 1 title
+    if len(title_list) == 0:
+        log_msg = "No Title defined for Indicator ID %s"
+        logging.error(log_msg, indic_id)
+        return
+    elif len(title_list) == 1:
+        title = title_list[0][0]
+    else:
+        log_msg = "Multiple titles (?) defined for Indicator ID %s"
+        logging.error(log_msg, len(title_list), indic_id)
+        return
+    # OK, 1 title found. Convert it to a name
+    name = 'dmow-ind' + str(indic_id).zfill(3) + '-' + title
+    name = re.sub('[^0-9a-zA-Z_\-]', '_', name).lower()
+    logging.info("Name: %s", name)
+    my_param = {
+        'name': name,
+        'title': title,
+    }
+    my_param['owner_org'] = config['OpenData']['owner_org']
+    my_param['license_id'] = config['OpenData']['license_id']
+    logging.debug("Parameters: %s", my_param)
+    try:
+        pkg = ckan_conn.action.package_create(**my_param)
+    except:
+        e = sys.exc_info()[1]
+        ec = sys.exc_info()[0]
+        log_msg = "Package Create not successful %s %s"
+        logging.error(log_msg, e, ec)
+    else:
+        # Collect Package information and set it in the database.
+        try:
+            val_id = pkg['id']
+        except:
+            log_msg = "Create package with no errors, but no package ID found..."
+            logging.error(log_msg)
+            return
+        else:
+            # Store package ID in indicators table
+            query = "INSERT INTO indicators (indicator_id, attribute, value, created) " \
+                    "VALUES (?, 'id', ?, ?)"
+            try:
+                cur.execute(query, (indic_id, val_id, now))
+            except:
+                log_msg = "Insert query failed **%s**, indic_id: %s, Value_id: %s"
+                logging.error(log_msg, query, indic_id, val_id)
+                return
+            conn.commit()
+        log_msg = 'Looks like I have my package...'
+        logging.info(log_msg)
+    log_msg = "End of create_package processing for Indicator %s"
+    logging.info(log_msg, indic_id)
+
+
+def update_package(indic_id, ckan_conn):
+    """
+    This procedure will update Package information for the indicator ID.
+    :param indic_id:
+    :return:
+    """
+    params = {}
+    log_msg = "Update Package for Indicator %s"
+    logging.info(log_msg, indic_id)
+    # Get ID of the package
+    query = "SELECT value FROM indicators WHERE attribute = 'id' AND indicator_id = ?"
+    cur.execute(query, (indic_id,))
+    res = cur.fetchone()
+    params['id'] = res[0]  # Remember ID of the package in params dictionary
+    # Now get extra fields that need to be populated
+    # First get attribute names for extra fields
+    query = "SELECT attribute, od_field FROM attribute_action WHERE action = 'Extra'"
+    cur.execute(query)
+    res = cur.fetchall()
+    # Remember the attribute - od_field translation
+    od_field = {}
+    for [k, v] in res:
+        od_field[k] = v
+    # Then get values for the attribute names
+    attribs = [res[i][0] for i in range(len(res))]
+    # With attribute names, find corresponding values in indicators table
+    query = "SELECT attribute, value FROM indicators WHERE indicator_id = ? AND attribute IN " + str(tuple(attribs))
+    logging.debug("Query: %s", query)
+    cur.execute(query, (indic_id,))
+    res = cur.fetchall()
+    extra_arr = []
+    for [k, v] in res:
+        attrib_dict = {
+            'key': od_field[k],  # Use human readable label as key
+            'value': v
+        }
+        extra_arr.append(attrib_dict)
+    # Add extras dictionary to params dictionary
+    params['extras'] = extra_arr
+    # Then get attribute names for Main fields
+    query = "SELECT attribute, od_field FROM attribute_action " \
+            "WHERE action = 'Main' AND source = 'Dataroom' AND target = 'Dataset'"
+    cur.execute(query)
+    res = cur.fetchall()
+    # Remember the attribute - od_field translation
+    od_field = {}
+    for [k, v] in res:
+        od_field[k] = v
+    # Then get values for the attribute names
+    attribs = [res[i][0] for i in range(len(res))]
+    # With attribute names, find corresponding values in indicators table
+    query = "SELECT attribute, value FROM indicators WHERE indicator_id = ? AND attribute IN " + str(tuple(attribs))
+    logging.debug("Query: %s", query)
+    cur.execute(query, (indic_id,))
+    res = cur.fetchall()
+    # Add extras dictionary to params dictionary
+    for [k, v] in res:
+        params[od_field[k]] = v
+    print("%s" % params)
+    log_msg = "Trying to update package"
+    logging.debug(log_msg)
+    try:
+        pkg = ckan_conn.action.package_update(**params)
+    except:
+        e = sys.exc_info()[1]
+        ec = sys.exc_info()[0]
+        log_msg = "Package Update not successful %s %s"
+        logging.error(log_msg, e, ec)
+    else:
+        log_msg = "Package Update successful %s"
+        logging.info(log_msg, pkg)
+    return
+
+
+def get_ckan_conn():
+    """
+    Configure the connection to ckan Open Data Platform.
+    :return:
+    """
+    logging.debug("Setup connection to ckan Server")
+    url = config['CKANServer']['url']
+    api = config['CKANServer']['api']
+    try:
+        ckan_conn = ckanapi.RemoteCKAN(url, apikey=api)
+    except:
+        e = sys.exc_info()[1]
+        ec = sys.exc_info()[0]
+        log_msg = "Connect to RemoteCKAN not successful %s %s"
+        logging.critical(log_msg, e, ec)
+        sys.exit(1)
+    return ckan_conn
+
+
+def scan_for_files():
     """
     Function to scan input directory for new files in sequence: commentaar - cijfers - metadata.
     If a file is found, call file handling procedure.
-    :param config: Read scan directory and handled directory from config file.
     :return:
     """
     scandir = config['Main']['scandir']
@@ -187,42 +453,53 @@ def scan_for_files(config):
     logging.debug(log_msg, scandir)
     log_msg = "First get my FTP Connection"
     logging.debug(log_msg)
-    host = config['FTPServer']['host']
-    user = config['FTPServer']['user']
-    passwd = config['FTPServer']['passwd']
-    ftp = ftp_connection(host, user, passwd)
+    ftp = ftp_connection()
     # Don't use os.listdir in for loop since I'll move files. For loop will get confused.
     # Extract filelist first.
-    filelist = [file for file in os.listdir(scandir) if 'cijfers' or 'commentaar' in file]
+    filelist = [file for file in os.listdir(scandir) if ('cijfers' in file) or ('commentaar' in file)]
     for file in filelist:
-        indic_id = indic_from_file(file)
-        print("Filename: %s, Indicator ID: %s" % (file, indic_id))
+        log_msg = "Filename: %s"
+        logging.debug(log_msg, file)
         move_file(file, scandir, handledir)  # Move file done in own function, such a hassle...
         if 'empty' in file:
             remove_file(ftp, file=os.path.join(handledir, file))
         else:
             load_file(ftp, file=os.path.join(handledir, file))
+        url_in_db(file)
     # Now close FTP Object
     ftp.close()
+    # Now handle meta-data
+    # Get ckan connection first
+    ckan_conn = get_ckan_conn()
+    filelist = [file for file in os.listdir(scandir) if 'metadata' in file]
+    for file in filelist:
+        log_msg = "Filename: %s"
+        logging.debug(log_msg, file)
+        move_file(file, scandir, handledir)  # Move file done in own function, such a hassle...
+        if 'empty' in file:
+            # remove_metadata(file)
+            pass
+        else:
+            indic_id = indic_from_file(file)
+            filename = os.path.join(handledir, file)
+            load_metadata(filename, indic_id, ckan_conn)
     return
 
 
-def main():
-    # Get ini-file first.
-    modulename = get_modulename()
-    config = get_inifile()
-    # Now configure logfile
-    logfilename = get_logfilename(modulename, config)
-    logging.basicConfig(format='%(asctime)s:%(module)s:%(funcName)s:%(lineno)d:%(levelname)s:%(message)s',
-                        datefmt='%d/%m/%Y %H:%M:%S', filename=logfilename, level=logging.DEBUG)
-    logging.info('Start Application')
-    scan_for_files(config)
-    logging.info('Call read_metadata module')
-    # Mod.read_metadata('C:/Projects/Vo/MOW Dataroom/OpenData/od_xml.xml')
-    logging.info('FTP Server Handling')
-    # Mr.load_file('C:/Projects/Vo/MOW Dataroom/OpenData/cijfers_11.xml')
-    logging.info('End Application')
+# Get ini-file first.
+modulename = get_modulename()
+config = get_inifile()
+# Now configure logfile
+logfilename = get_logfilename()
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(module)s:%(funcName)s:%(lineno)d:%(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S', filename=logfilename, level=logging.DEBUG)
+logging.info('\n\n\nStart Application')
+# Get my database connection
+conn = connect2db()
+cur = conn.cursor()
+scan_for_files()
+# Close database connection
+conn.close()
+logging.info('End Application')
 
 
-if __name__ == '__main__':
-    main()
